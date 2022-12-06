16c0ed1e539d8be56e647ddb388f57360686a5b7
Marc Capde <capasc5@gmail.com>
2022-12-06 19:24:32 +0100

    form

---
+++ app/src/main/java/com/example/move_t/EventForm.java
@@ -0,0 +1,14 @@
+package com.example.move_t;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventForm extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.event_form);
+  }
+}
